a8cf5ca776e16289346c3104db739ef83e3abe64
jukaijoestar <135386955+jukaijoestar@users.noreply.github.com>
2024-06-22 08:58:56 -0500

    0.6.3
    Tablas agregadas en la base de datos

--- sql/comite_sena.sql
+++ sql/comite_sena.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 21-06-2024 a las 18:26:40
+-- Tiempo de generación: 22-06-2024 a las 15:39:47
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -46,12 +46,9 @@ CREATE TABLE `aprendiz` (
 --
 
 INSERT INTO `aprendiz` (`ID_Aprendiz`, `Numero_Documento`, `Tipo_Documento`, `Primer_Nombre`, `Segundo_Nombre`, `Primer_Apellido`, `Segundo_Apellido`, `Email`, `Telefono`, `ID_Ficha`, `Imagen`) VALUES
-(49, '1048264905', 'CE', 'omar', 'Daviddddddddd', 'Camargo', 'Narvaez', 'stivencito123@gmail.com', '3007097628', 16, 'R.jpg'),
-(59, '10828352977', 'CC', 'Steven', 'raul', 'alfonso', 'mosquera', 'omar123@gmail.com', '2017281410', 0, '3155cae422f11de4a94b92337751b0d1.jpg'),
-(60, '10828352977', 'CC', 'Steven', 'raul', 'alfonso', 'mosquera', 'omar123@gmail.com', '2017281410', 0, '3155cae422f11de4a94b92337751b0d1.jpg'),
-(61, '1048264905', 'CC', 'Omar', 'Alfonso', 'Camargo', 'Mosquera', 'stivencito123@gmail.com', '3007097628', 18, 'descarga.png'),
-(63, '1048264905', 'CC', 'Antony ', 'Osorio', 'Camargo', 'Mosquera', 'stivencito123@gmail.com', '3007097628', 16, '3155cae422f11de4a94b92337751b0d1 (1).jpg'),
-(66, '1048264905', 'TI', 'esteban', 'Osorio', 'Camargo', 'Mosquera', 'stivencito123@gmail.com', '3007097628', 17, '3155cae422f11de4a94b92337751b0d1 (1).jpg');
+(1, '1234', 'CC', 'A', 'B', 'C', 'D', 'qw@gmail.com', '123', 19, 'GQXgy08bAAA2zFW.png'),
+(2, '22222', 'CC', 'we', 'wqe', 'qwe', 'qwe', 'qewewq@gmail.com', '123', 23, 'GQXgy08bAAA2zFW.png'),
+(3, '1233333333333333', 'CC', '12333', '33', 'ss', 'ss', 'ss@gmail.com', '123', 23, 'GQXgy08bAAA2zFW.png');
 
 -- --------------------------------------------------------
 
@@ -112,74 +109,27 @@ CREATE TABLE `comite` (
   `num` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Volcado de datos para la tabla `comite`
---
-
-INSERT INTO `comite` (`id`, `id_aprendiz`, `id_ficha`, `descripcion`, `num`) VALUES
-(1, 1, 1, 'cambiar', 1),
-(2, 1, 1, 'adasdsss', 2),
-(4, 1, 1, 'sddddddd', 1),
-(6, 1, 1, 'fffffffffffffhhh', 1),
-(7, 1, 1, 'ewqeqweqqq', 2),
-(8, 5, 1, 'el,wpfomwepofmwpofmwoemffwfo ahhahahahah', 1),
-(9, 10, 11, 'se porta demasiado mal ', 2),
-(10, 1, 1, 'el aprendiz debe ser remitido a bienestar', 1),
-(11, 1, 16, 'esta persona fue aplazada', 2),
-(12, 1, 16, 'esta persona fue expulada', 6),
-(13, 1, 16, 'lo en encontraron vendiendo perico en los baños', 5),
-(14, 22, 17, 'esta persona se porta muy mal en clases y no asiste', 1),
-(15, 22, 17, 'lo mismo que el comite pasado', 2),
-(16, 22, 17, 'no mijo todavia?', 3),
-(17, 22, 17, 'vamos es a sacarte del sena', 4),
-(18, 22, 17, 'vendiendo?', 5),
-(19, 22, 17, 'comprando?', 6),
-(20, 46, 16, 'gggg', 1),
-(21, 53, 17, 'esta persona tiene que entregar un informe completo', 5),
-(22, 54, 21, 'esta persona fue aplazada', 1),
-(23, 54, 21, 'esta persona fue encontrada metiendo vicio', 2),
-(24, 36, 16, 'que es la vaina mija', 5),
-(25, 36, 16, 'aja y entonces', 6),
-(26, 36, 16, 'malooo', 2),
-(27, 36, 16, 'locooo', 3),
-(28, 52, 16, 'locooo', 5),
-(29, 52, 16, 'locooo', 2),
-(30, 52, 16, 'klk', 6),
-(31, 52, 16, 'klkl mamawebo', 6),
-(32, 52, 16, 'ajaaaaaaaaaaaaaaa', 2),
-(33, 47, 16, 'este man tenia una gorra', 3),
-(34, 63, 16, 'este man tiene el dedo mocho', 1),
-(35, 66, 17, 'esteban va para comite', 1);
-
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `comite_general`
+-- Estructura de tabla para la tabla `comite_extraordinario`
 --
 
-CREATE TABLE `comite_general` (
-  `id` int(11) NOT NULL,
-  `nombre` varchar(255) NOT NULL,
-  `fecha` date NOT NULL,
-  `hora_inicio` time NOT NULL,
-  `hora_fin` time NOT NULL,
-  `puntos_destacados` text NOT NULL,
-  `objetivos` text NOT NULL,
-  `ID_Ficha` int(11) NOT NULL,
-  `desarrollo` text NOT NULL,
-  `asistentes` varchar(255) NOT NULL,
-  `actividad` text NOT NULL,
-  `responsable` text NOT NULL,
-  `fecha_compromiso` date DEFAULT NULL
+CREATE TABLE `comite_extraordinario` (
+  `ID_extraordinario` int(11) NOT NULL,
+  `Acta_Num` varchar(10) NOT NULL,
+  `Nombre` varchar(200) NOT NULL,
+  `Fecha` date NOT NULL DEFAULT current_timestamp(),
+  `Hora_inicio` time NOT NULL,
+  `Hora_fin` time NOT NULL,
+  `Agendas` varchar(500) NOT NULL,
+  `Objetivo` varchar(500) NOT NULL,
+  `Desarrollo` varchar(500) NOT NULL,
+  `Actividad` varchar(500) NOT NULL,
+  `Responsable` varchar(600) NOT NULL,
+  `ruta_foto` varchar(300) NOT NULL,
+  `ID_ficha` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Volcado de datos para la tabla `comite_general`
---
-
-INSERT INTO `comite_general` (`id`, `nombre`, `fecha`, `hora_inicio`, `hora_fin`, `puntos_destacados`, `objetivos`, `ID_Ficha`, `desarrollo`, `asistentes`, `actividad`, `responsable`, `fecha_compromiso`) VALUES
-(3, 'sancion ', '2024-06-06', '12:00:00', '12:00:00', 'hhhhhhhhhggg', 'hhhh', 2, '', '', '', '', NULL),
-(16, 'sancion 23', '2024-06-06', '17:44:00', '21:41:00', 'nhhhhhhh', 'hjjkjkjk', 1, '', '', '', '', NULL);
 
 -- --------------------------------------------------------
 
@@ -207,7 +157,8 @@ INSERT INTO `ficha` (`ID_Ficha`, `Numero_Ficha`, `Nombre_Ficha`, `modalidad_id`,
 (19, 456789, 'Administración de Empresas Agropecuarias', 1, 1, 4),
 (20, 567890, 'Ingeniería de Sistemas', 2, 2, 5),
 (21, 12345678, 'sistemas', 1, 2, 27),
-(22, 12121212, 'construccion', 1, 2, 35);
+(22, 12121212, 'construccion', 1, 2, 35),
+(23, 1235, 'Ciencias cientificas', 1, 1, 35);
 
 -- --------------------------------------------------------
 
@@ -247,6 +198,19 @@ CREATE TABLE `modalidad` (
 INSERT INTO `modalidad` (`id`, `nombre`) VALUES
 (1, 'presencial'),
 (2, 'virtual');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `observaciones3`
+--
+
+CREATE TABLE `observaciones` (
+  `ID` int(11) NOT NULL,
+  `Contenido` varchar(500) NOT NULL,
+  `ID_Aprendiz` int(11) NOT NULL,
+  `ID_extraordinario` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -347,11 +311,10 @@ ALTER TABLE `comite`
   ADD KEY `id_ficha` (`id_ficha`);
 
 --
--- Indices de la tabla `comite_general`
+-- Indices de la tabla `comite_extraordinario`
 --
-ALTER TABLE `comite_general`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `fk_id_ficha` (`ID_Ficha`);
+ALTER TABLE `comite_extraordinario`
+  ADD PRIMARY KEY (`ID_extraordinario`);
 
 --
 -- Indices de la tabla `ficha`
@@ -375,6 +338,12 @@ ALTER TABLE `modalidad`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indices de la tabla `observaciones3`
+--
+ALTER TABLE `observaciones3`
+  ADD PRIMARY KEY (`ID`);
+
+--
 -- Indices de la tabla `regional`
 --
 ALTER TABLE `regional`
@@ -394,7 +363,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT de la tabla `aprendiz`
 --
 ALTER TABLE `aprendiz`
-  MODIFY `ID_Aprendiz` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=67;
+  MODIFY `ID_Aprendiz` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de la tabla `centro`
@@ -406,19 +375,19 @@ ALTER TABLE `centro`
 -- AUTO_INCREMENT de la tabla `comite`
 --
 ALTER TABLE `comite`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT de la tabla `comite_general`
+-- AUTO_INCREMENT de la tabla `comite_extraordinario`
 --
-ALTER TABLE `comite_general`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+ALTER TABLE `comite_extraordinario`
+  MODIFY `ID_extraordinario` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `ficha`
 --
 ALTER TABLE `ficha`
-  MODIFY `ID_Ficha` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+  MODIFY `ID_Ficha` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 
 --
 -- AUTO_INCREMENT de la tabla `formacion`
@@ -431,6 +400,12 @@ ALTER TABLE `formacion`
 --
 ALTER TABLE `modalidad`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `observaciones3`
+--
+ALTER TABLE `observaciones3`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `regional`
@@ -453,14 +428,6 @@ ALTER TABLE `users`
 --
 ALTER TABLE `centro`
   ADD CONSTRAINT `centro_ibfk_1` FOREIGN KEY (`regional_id`) REFERENCES `regional` (`id`);
-
---
--- Filtros para la tabla `ficha`
---
-ALTER TABLE `ficha`
-  ADD CONSTRAINT `ficha_ibfk_1` FOREIGN KEY (`modalidad_id`) REFERENCES `modalidad` (`id`),
-  ADD CONSTRAINT `ficha_ibfk_2` FOREIGN KEY (`formacion_id`) REFERENCES `formacion` (`id`),
-  ADD CONSTRAINT `ficha_ibfk_3` FOREIGN KEY (`centro_id`) REFERENCES `centro` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
